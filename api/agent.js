7d4014f8eb71ffe3c81ec465ea6f9fd16d5e369e
RalfLoskill <ralf.loskill@googlemail.com>
2026-05-04 15:04:03 +0200

    Create agent.js

---
+++ api/agent.js
@@ -0,0 +1,11 @@
+
+  "name": "toni-lernagent",
+  "version": "1.0.0",
+  "scripts": {
+    "dev": "vercel dev",
+    "deploy": "vercel --prod"
+  },
+  "devDependencies": {
+    "vercel": "^52.0.0"
+  }
+}
