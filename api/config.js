304de4920ac7ba742ddee3cbaa672dcc9f9b7cb2
RalfLoskill <ralf.loskill@googlemail.com>
2026-05-27 16:01:17 +0200

    Create config.js

---
+++ api/config.js
@@ -0,0 +1,8 @@
+export default function handler(req, res) {
+  res.setHeader('Content-Type', 'application/javascript');
+  res.setHeader('Cache-Control', 'no-store');
+  res.send(`
+    window.SUPABASE_URL = "${process.env.SUPABASE_URL}";
+    window.SUPABASE_ANON_KEY = "${process.env.SUPABASE_ANON_KEY}";
+  `);
+}
